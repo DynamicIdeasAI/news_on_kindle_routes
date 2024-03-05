d5359652016208f70e413fbf4ec9ebb52539404e
Capt. Michael <CaptMichaelDev@gmail.com>
2024-03-04 20:08:12 -0500

    feat: route type of post

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './routes';
 export * from './type/channel.type';
 export * from './type/category.type';
+export * from './type/post.type';
---
+++ src/type/post.type.ts
@@ -0,0 +1,60 @@
+import type { BaseRouteType, BaseRequestType, BaseResponseType } from '@dynamicideas/base-types';
+import type { PostDataType } from '@dynamicideas/news-on-kindle-data-types';
+
+interface PostRouteType extends BaseRouteType {
+  get: PostGetRequestType;
+  post: PostPostRequestType;
+  put: PostPutRequestType;
+  delete: PostDeleteRequestType;
+}
+
+interface PostGetRequestType extends BaseRequestType {
+  /**
+   * Page index, starting from 0.
+   */
+  pageIndex: number;
+  pageSize: number;
+  channelId?: string;
+  ids?: string[];
+  isDeleted?: boolean;
+}
+
+interface PostGetResponseType extends BaseResponseType {
+  data: PostDataType[];
+}
+
+interface PostPostRequestType extends BaseRequestType {
+  data: PostDataType;
+}
+
+interface PostPostResponseType extends BaseResponseType {
+  data: PostDataType;
+}
+
+interface PostPutRequestType extends BaseRequestType {
+  data: PostDataType;
+}
+
+interface PostPutResponseType extends BaseResponseType {
+  data: PostDataType;
+}
+
+interface PostDeleteRequestType extends BaseRequestType {
+  ids: string[];
+}
+
+interface PostDeleteResponseType extends BaseResponseType {
+  data: PostDataType[];
+}
+
+export type {
+  PostRouteType,
+  PostGetRequestType,
+  PostGetResponseType,
+  PostPostRequestType,
+  PostPostResponseType,
+  PostPutRequestType,
+  PostPutResponseType,
+  PostDeleteRequestType,
+  PostDeleteResponseType
+};
